0b74b558d985367208ed86f416ab4dbff46c2846
Felix Dorner <mail@felixdorner.com>
2015-09-17 20:33:25 +0200

    TGM Plugin Adjustments and Update
    Required for WordPress.org Guideline

--- inc/plugins/plugin-activation.php
+++ inc/plugins/plugin-activation.php
@@ -8,50 +8,54 @@
  */
 
 require_once dirname( __FILE__ ) . '/class-tgm-plugin-activation.php';
+add_action( 'tgmpa_register', 'kouki_register_required_plugins' );
+/**
+ * Register the required plugins for this theme.
+ *
+ * In this example, we register five plugins:
+ * - one included with the TGMPA library
+ * - two from an external source, one from an arbitrary source, one from a GitHub repository
+ * - two from the .org repo, where one demonstrates the use of the `is_callable` argument
+ *
+ * The variable passed to tgmpa_register_plugins() should be an array of plugin
+ * arrays.
+ *
+ * This function is hooked into tgmpa_init, which is fired within the
+ * TGM_Plugin_Activation class constructor.
+ */
 function kouki_register_required_plugins() {
-
+  /*
+   * Array of plugin arrays. Required keys are name and slug.
+   * If the source is NOT from the .org repo, then source is also required.
+   */
   $plugins = array(
-
+    // This is an example of how to include a plugin bundled with a theme.
     array(
-      'name'      => 'Options Framework',
-      'slug'      => 'options-framework',
-      'required'  => false,
-      'force_activation' => false,
-    )    
-
+      'name'               => 'Options Framework', // The plugin name.
+      'slug'               => 'options-framework', // The plugin slug (typically the folder name).      
+      'required'           => false, // If false, the plugin is only 'recommended' instead of required.      
+    )
   );
-
+  /*
+   * Array of configuration settings. Amend each line as needed.
+   *
+   * TGMPA will start providing localized text strings soon. If you already have translations of our standard
+   * strings available, please help us make TGMPA even better by giving us access to these translations or by
+   * sending in a pull-request with .po file(s) with the translations.
+   *
+   * Only uncomment the strings in the config array if you want to customize the strings.
+   */
   $config = array(
-    'domain'       => 'kouki',
+    'id'           => 'tgmpa',                 // Unique ID for hashing notices for multiple instances of TGMPA.
+    'default_path' => '',                      // Default absolute path to bundled plugins.
     'menu'         => 'tgmpa-install-plugins', // Menu slug.
+    'parent_slug'  => 'themes.php',            // Parent menu slug.
+    'capability'   => 'edit_theme_options',    // Capability needed to view plugin install page, should be a capability associated with the parent menu used.
     'has_notices'  => true,                    // Show admin notices or not.
     'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
     'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
-    'is_automatic' => true,                   // Automatically activate plugins after installation or not.
+    'is_automatic' => false,                   // Automatically activate plugins after installation or not.
     'message'      => '',                      // Message to output right before the plugins table.
-    'strings'      => array(
-        'page_title'                      => __( 'Install Required Plugins', 'kouki' ),
-        'menu_title'                      => __( 'Install Plugins', 'kouki' ),
-        'installing'                      => __( 'Installing Plugin: %s', 'kouki' ), // %s = plugin name.
-        'oops'                            => __( 'Something went wrong with the plugin API.', 'kouki' ),
-        'notice_can_install_required'     => _n_noop( 'This theme requires the following plugin: %1$s.', 'This theme requires the following plugins: %1$s.' ), // %1$s = plugin name(s).
-        'notice_can_install_recommended'  => _n_noop( 'This theme recommends the following plugin: %1$s.', 'This theme recommends the following plugins: %1$s.' ), // %1$s = plugin name(s).
-        'notice_cannot_install'           => _n_noop( 'Sorry, but you do not have the correct permissions to install the %s plugin. Contact the administrator of this site for help on getting the plugin installed.', 'Sorry, but you do not have the correct permissions to install the %s plugins. Contact the administrator of this site for help on getting the plugins installed.' ), // %1$s = plugin name(s).
-        'notice_can_activate_required'    => _n_noop( 'The following required plugin is currently inactive: %1$s.', 'The following required plugins are currently inactive: %1$s.' ), // %1$s = plugin name(s).
-        'notice_can_activate_recommended' => _n_noop( 'The following recommended plugin is currently inactive: %1$s.', 'The following recommended plugins are currently inactive: %1$s.' ), // %1$s = plugin name(s).
-        'notice_cannot_activate'          => _n_noop( 'Sorry, but you do not have the correct permissions to activate the %s plugin. Contact the administrator of this site for help on getting the plugin activated.', 'Sorry, but you do not have the correct permissions to activate the %s plugins. Contact the administrator of this site for help on getting the plugins activated.' ), // %1$s = plugin name(s).
-        'notice_ask_to_update'            => _n_noop( 'The following plugin needs to be updated to its latest version to ensure maximum compatibility with this theme: %1$s.', 'The following plugins need to be updated to their latest version to ensure maximum compatibility with this theme: %1$s.' ), // %1$s = plugin name(s).
-        'notice_cannot_update'            => _n_noop( 'Sorry, but you do not have the correct permissions to update the %s plugin. Contact the administrator of this site for help on getting the plugin updated.', 'Sorry, but you do not have the correct permissions to update the %s plugins. Contact the administrator of this site for help on getting the plugins updated.' ), // %1$s = plugin name(s).
-        'install_link'                    => _n_noop( 'Begin installing plugin', 'Begin installing plugins' ),
-        'activate_link'                   => _n_noop( 'Begin activating plugin', 'Begin activating plugins' ),
-        'return'                          => __( 'Return to Required Plugins Installer', 'kouki' ),
-        'plugin_activated'                => __( 'Plugin activated successfully.', 'kouki' ),
-        'complete'                        => __( 'All plugins installed and activated successfully. %s', 'kouki' ), // %s = dashboard link.
-        'nag_type'                        => 'updated' // Determines admin notice type - can only be 'updated', 'update-nag' or 'error'.
-      )
   );
-
   tgmpa( $plugins, $config );
-
 }
-add_action( 'tgmpa_register', 'kouki_register_required_plugins' );
